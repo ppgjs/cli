6af70dbd73ae7ee619aa3d44d32760c8d7d07b65
BoycePan <pz886688@163.com>
2026-07-16 22:10:33 +0800

    chore(projects): release v0.0.40

--- src/command/open-git-store.ts
+++ src/command/open-git-store.ts
@@ -5,6 +5,14 @@ export async function openStore(wrapUrL = '') {
   let openUrl = wrapUrL;
   if (!openUrl) openUrl = await execCommand('git', ['remote', 'get-url', GitInfo.useRemote]);
   if (!openUrl) return logError('没有可打开的网址(＾－＾)');
+
+  if (openUrl.startsWith('git@')) {
+    openUrl = openUrl.replace(/^git@([^:]+):/, 'https://$1/');
+  } else if (openUrl.startsWith('ssh://')) {
+    openUrl = openUrl.replace(/^ssh:\/\/(?:[^@]+@)?([^/]+)\//, 'https://$1/');
+  }
+  openUrl = openUrl.replace(/\.git$/, '');
+
   const open = await import('open');
   logInfo(`打开地址: ${openUrl}`);
   try {
